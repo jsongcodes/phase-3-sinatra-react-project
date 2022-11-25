7bacef2f72e2714d866d4f9d8b236c5d8475605d
Jasmin <ssongnari@gmail.com>
2022-11-25 17:07:05 -0500

    Created users table

--- db/migrate/20221125214516_create_users.rb
+++ db/migrate/20221125214516_create_users.rb
@@ -1,6 +1,10 @@
 class CreateUsers < ActiveRecord::Migration[6.1]
   def change
-
+    create_table users do |t|
+      t.string :name
+      t.integer :years_of_experience
+      datetime :created_at
+      datetime :updated_at
     end
   end
 end
