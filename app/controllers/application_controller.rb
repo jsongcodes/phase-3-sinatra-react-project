a64d9023562abc72b4d43aa220278aeb03ef6059
Jasmin <ssongnari@gmail.com>
2022-11-28 19:24:00 -0500

    Added get, post,  patch, delete requests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,48 @@ class ApplicationController < Sinatra::Base
     { message: "Good luck with your project!" }.to_json
   end
 
+  get '/plants' do
+    Plant.all.to_json
+  end
+
+  # get '/games/:id' do
+  #   game = Game.find(params[:id])
+
+  #   game.to_json(only: [:id, :title, :genre, :price], include: {
+  #     reviews: { only: [:comment, :score], include: {
+  #       user: { only: [:name] }
+  #     } }
+  #   })
+  # end
+
+  post "/plants" do
+    Plant.create(
+      name: params[:name],
+      directions: params[:directions],
+      user_id: params[:user_id],
+      date: params[:date],
+      time: params[:time]
+    ).to_json
+  end
+
+  patch "/plants/:id" do 
+    Plant.find(params[:id]).update(
+      name: params[:name],
+      directions: params[:directions],
+      user_id: params[:user_id],
+      date: params[:date],
+      time: params[:time]
+    ).to_json
+end
+
+  delete '/plants/:id' do
+      # find the plant using the ID
+    plant = Plant.find(params[:id])
+      # delete the plant
+    plant.destroy
+      # send a response with the deleted plant as JSON
+    plant.to_json
+  end
+
+
 end
